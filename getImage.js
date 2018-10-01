433156f50b75faeca9aa1c369c0eb0baaf2d24be
Louis Riehl <louisriehl@gmail.com>
2018-10-01 17:43:57 +0000

    getImage fetches and downloads image

---
+++ getImage.js
@@ -0,0 +1,14 @@
+// console.log('npmstart is working!');
+
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+  .on('error', err => {
+    throw err;
+  })
+  .on('response', response => {
+    console.log('Status code:', response.statusMessage);
+    console.log('Content type: ', response.headers['content-type']);
+  })
+  .pipe(fs.createWriteStream('./future.jpeg'));

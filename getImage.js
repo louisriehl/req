cd29bc7268cd67e5a15ed67144d61fee18497a8a
Louis Riehl <louisriehl@gmail.com>
2018-10-01 17:47:31 +0000

    added download messages

--- getImage.js
+++ getImage.js
@@ -3,6 +3,7 @@
 var request = require('request');
 var fs = require('fs');
 
+console.log('Downloading image...');
 request.get('https://sytantris.github.io/http-examples/future.jpg')
   .on('error', err => {
     throw err;
@@ -11,4 +12,7 @@ request.get('https://sytantris.github.io/http-examples/future.jpg')
     console.log('Status code:', response.statusMessage);
     console.log('Content type: ', response.headers['content-type']);
   })
-  .pipe(fs.createWriteStream('./future.jpeg'));
+  .pipe(fs.createWriteStream('./future.jpeg'))
+  .on('end', () => {
+    console.log('Download complete!');
+  });
